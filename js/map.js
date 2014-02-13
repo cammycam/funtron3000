b301f610fc075a827306a1373c96aec5a4312d7f
Justin Maillet <maillet@cs.dal.ca>
2014-02-12 21:52:08 -0400

    changed to use jquery and comply with jshint & jslint

--- js/map.js
+++ js/map.js
@@ -4,17 +4,15 @@ function initialize() {
 
     //get location
     if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(drawMap,showError);
-		}
-    else
-        document.getElementById('map_canvas').innerHTML = "Error with geolocation.";
+        navigator.geolocation.getCurrentPosition(drawMap, showError);
+    } else {
+        $('#map_canvas')[0].innerHTML = "Your browser does not support geolocation.";
+    }
 }
 
 function drawMap(position) {
 
     "use strict";
-	
-	document.getElementById('map_canvas').innerHTML = "DERP3";
 
     var map = new google.maps.Map($('#map_canvas')[0], {
             center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
@@ -38,26 +36,26 @@ function drawMap(position) {
                 elementType: 'geometry',
                 stylers: [{ hue: '#fff700' }, { lightness: -15 }, { saturation: 99 }]
             }]);
-
 }
 
-function showError(error)
-  {
-  switch(error.code) 
-    {
+function showError(error) {
+
+    "use strict";
+    
+    switch (error.code) {
     case error.PERMISSION_DENIED:
-      document.getElementById('map_canvas').innerHTML="User denied the request for Geolocation."
-      break;
+        $('#map_canvas')[0].innerHTML = "User denied the request for Geolocation.";
+        break;
     case error.POSITION_UNAVAILABLE:
-      document.getElementById('map_canvas').innerHTML="Location information is unavailable."
-      break;
+        $('#map_canvas')[0].innerHTML = "Location information is unavailable.";
+        break;
     case error.TIMEOUT:
-      document.getElementById('map_canvas').innerHTML="The request to get user location timed out."
-      break;
+        $('#map_canvas')[0].innerHTML = "The request to get user location timed out.";
+        break;
     case error.UNKNOWN_ERROR:
-      document.getElementById('map_canvas').innerHTML="An unknown error occurred."
-      break;
+        $('#map_canvas')[0].innerHTML = "An unknown error occurred.";
+        break;
     }
-  }
-  
+}
+
 google.maps.event.addDomListener(window, 'load', initialize);
